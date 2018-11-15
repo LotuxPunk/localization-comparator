e27a26830a94250843b734b1574e167428b7c975
LotuxPunk <clement@vandendaelen.com>
2018-11-15 23:23:23 +0100

    initial

---
+++ main.js
@@ -0,0 +1,4 @@
+document.getElementById("confirm").onclick = () => {
+    let original = document.getElementById("inputO").textContent;
+    let local = document.getElementById("inputL").textContent;
+}
